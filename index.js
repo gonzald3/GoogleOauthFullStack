1724482f4e2b3b382ac58b5e082d0e574bc5ed58
Daniel Gonzalez <gonzald3@protonmail.com>
2023-09-12 10:36:54 -0400

    app get req user route authentication

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 //common js for sever because the code is older
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
 //order does matter for Users model and access of database
@@ -9,6 +11,15 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 //runs the authRoutes file and returns the function 
 //immediately calling it with the app var
