33157339b534fca0f6c58c7a851c26e475af6b5a
Daniel Gonzalez <gonzald3@protonmail.com>
2023-10-05 22:38:10 -0400

    authReducerReturnValues

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,6 +4,6 @@ import { FETCH_USER } from './types';
 //refactored for compact look and feel but better pattern
 export const fetchUser = () => async dispatch => {
         const res = await axios.get('/api/current_user');
-        dispatch({ type: FETCH_USER, payload: res});
+        dispatch({ type: FETCH_USER, payload: res.data});
 };
 
