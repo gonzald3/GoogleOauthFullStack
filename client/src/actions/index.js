a0677ec795cda2ec21e292f5d86da84f7bb06454
Daniel Gonzalez <gonzald3@protonmail.com>
2023-10-05 22:14:23 -0400

    refactored for compact look

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,12 +1,9 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-export const fetchUser = () => {
-    return function(dispatch) {
-        axios.get('/api/current_user')
-        .then(res => dispatch({ type: FETCH_USER, payload: res}));
-
-    }
-    
+//refactored for compact look and feel but better pattern
+export const fetchUser = () => async dispatch => {
+        const res = await axios.get('/api/current_user');
+        dispatch({ type: FETCH_USER, payload: res});
 };
 
