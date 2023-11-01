4453cd42c3aa408d45e463f1ab43fd1c2804b953
Daniel Gonzalez <gonzald3@protonmail.com>
2023-11-01 08:44:58 -0400

    charge / credit to account

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,10 +1,19 @@
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
 
+//Note: Use a test payment such as 4242 4242 4242 4242
+// 12/34      CVC: 567
 
 module.exports = app => {
-    app.post('/api/stripe', (req, res) => {
-        //console.log(req.body);
+    app.post('/api/stripe', async (req, res) => {
+        const charge = await stripe.charges.create({
+            amount: 500,
+            currency: 'usd',
+            description: '$5 for 5 credits',
+            source: req.body.id
+        });
+
+        console.log(charge);
 
 
     });
